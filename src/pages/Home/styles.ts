c8dfc93dbc32621e44c23fe7dc7508c5c4a34f09
HugoNCastro <hugonaaman@gmail.com>
2023-03-31 16:40:15 -0300

    Ajusta identação do styles.ts do componente Home

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -90,16 +90,20 @@ export const StartCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
-  border-radius: 8px:
+  border-radius: 8px;
+
+  display: flex;
   align-items: center;
   justify-content: center;
+
   gap: 0.5rem;
-  cursor: pointer;
   font-weight: bold;
+
+  cursor: pointer;
 
   background-color: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme['gray-100']};
-  
+
   &:not(:disabled):hover {
     background-color: ${(props) => props.theme['green-700']};
   }
@@ -108,5 +112,4 @@ export const StartCountdownButton = styled.button`
     opacity: 0.7;
     cursor: not-allowed;
   }
-
 `
