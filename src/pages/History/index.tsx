f9a172eb63b07f9249943b78a0611242ccaef388
HugoNCastro <hugonaaman@gmail.com>
2023-04-13 13:55:39 -0300

    Finaliza aplicação

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -27,7 +27,7 @@ export function History() {
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
                   <td>
-                    {formatDistanceToNow(cycle.startDate, {
+                    {formatDistanceToNow(new Date(cycle.startDate), {
                       addSuffix: true,
                       locale: ptBR,
                     })}
@@ -36,7 +36,7 @@ export function History() {
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluído</Status>
                     )}
-                    {cycle.finishedDate && (
+                    {cycle.interruptedDate && (
                       <Status statusColor="red">Interrompido</Status>
                     )}
                     {!cycle.finishedDate && !cycle.interruptedDate && (
